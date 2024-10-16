79a36a02096839e4bcbbd32f834420770f08cf2b
zuzia <zuzia@LAPTOP-EL6N6M48>
2024-10-16 09:18:32 +0200

    dodano modele

---
+++ Models/Wolontariusz.cs
@@ -0,0 +1,16 @@
+﻿namespace Schronisko.Models
+{
+    public class Wolontariusz
+    {
+        public int Id { get; set; }
+        public string Imie {  get; set; }
+        public string Nazwisko { get; set; }
+        public DateTime DataUrodzenia { get; set; }
+        public string Telefon {  get; set; }
+        public string Email { get; set; }
+        public bool Miasto { get; set; }
+        public string Opis { get; set; }
+        public bool Doswiadczenie { get; set; }
+        public string Dyspozycyjnosc {  get; set; }
+    }
+}
